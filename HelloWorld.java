2733cc79c8f1f011a917946a51c8a9edc98c0be1
Rohit Thaware <rthaware4@gmail.com>
2022-03-06 17:43:06 +0530

    HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+package tester;
+
+public class HelloWorld {
+
+	 private static void main(String[] args) {
+		System.out.println("Hello from Java");
+		System.out.println("Good Evening ,Rohit";
+		
+
+	}
+
+}
